64a2d24fcd9eb5fc3fab9e6f2cee3e0e09e7245b
HanBinGer <35628064+HanBinGer@users.noreply.github.com>
2024-06-09 11:48:55 +0300

    Add files via upload

---
+++ MiP.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+
+m = 10.0
+p = 0.1
+k = 3.0
+
+def y_dot(v):
+    return v
+
+def v_dot(y, v):
+    return -(p / m) * v - (k / m) * y
+
+def system(state, t):
+    y, v = state
+    dydt = v
+    dvdt = -(p/m) * v - (k/m) * y
+    return [dydt, dvdt]
+
+def check():
+    global V, Y, dt
+    solution = odeint(system, [1.0, 0.0], T)
+
+    y_ground_true = solution[:, 0]
+    v_ground_true = solution[:, 1]
+
+    if not np.allclose(y_ground_true, Y, atol=dt):
+        print("y not equal")
+    if not np.allclose(v_ground_true, V, atol=dt):
+        print("v not equal")
+
+x0 = -10.0
+x1 = 10.0
+dt = 0.001
+
+n = int((x1 - x0) / dt)
+T = np.linspace(x0, x1, n)
+Y = np.zeros(n)
+V = np.zeros(n)
+
+Y[0] = 1.0  # Начальные условия
+V[0] = 0.0
+
+for i in range(1, n):
+    k1_y = dt * y_dot(V[i-1])
+    k1_v = dt * v_dot(Y[i-1], V[i-1])
+    
+    k2_y = dt * y_dot(V[i-1] + 0.5 * k1_v)
+    k2_v = dt * v_dot(Y[i-1] + 0.5 * k1_y, V[i-1] + 0.5 * k1_v)
+    
+    k3_y = dt * y_dot(V[i-1] + 0.5 * k2_v)
+    k3_v = dt * v_dot(Y[i-1] + 0.5 * k2_y, V[i-1] + 0.5 * k2_v)
+    
+    k4_y = dt * y_dot(V[i-1] + k3_v)
+    k4_v = dt * v_dot(Y[i-1] + k3_y, V[i-1] + k3_v)
+    
+    Y[i] = Y[i-1] + (k1_y + 2*k2_y + 2*k3_y + k4_y) / 6
+    V[i] = V[i-1] + (k1_v + 2*k2_v + 2*k3_v + k4_v) / 6
+
+check()
+
+plt.figure(figsize=(12, 10))
+plt.plot(T, Y, label='y')
+plt.plot(T, V, label='dy/dt')
+plt.xlabel('t')
+
+plt.legend()
+plt.show()
